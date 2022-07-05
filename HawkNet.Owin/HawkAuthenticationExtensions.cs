459474d75a244c3665814de8d7b208afa6a35daf
Jonathan McEwan <jonathan.mcewan@briefbox.com.au>
2022-07-05 19:12:54 +1000

    small fixes of redundant references

--- HawkNet.Owin/HawkAuthenticationExtensions.cs
+++ HawkNet.Owin/HawkAuthenticationExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using Owin;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Owin.Extensions;
--- HawkNet.Owin/HawkAuthenticationOptions.cs
+++ HawkNet.Owin/HawkAuthenticationOptions.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Owin.Security;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
